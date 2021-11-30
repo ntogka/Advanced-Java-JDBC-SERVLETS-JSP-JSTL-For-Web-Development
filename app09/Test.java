0ef938def6851271edbdb727732447766892ff9f
Marina Ntogka <marinadoga98@gmail.com>
2021-11-30 17:13:06 +0200

    Add files via upload

---
+++ app09/Test.java
@@ -0,0 +1,42 @@
+package com.marina.app09;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Test {
+//Type-2 Driver Provided By Oracle
+	public static void main(String[] args) {
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:oci8:@xe", "system", "marina");
+			st = con.createStatement();
+			rs = st.executeQuery("select * from emp1");
+			System.out.println("ENO\tENAME\tESAL\tEADDR");
+			System.out.println("------------------------------");
+			while(rs.next()) {
+				System.out.print(rs.getInt("ENO") + "\t");
+				System.out.print(rs.getString("ENAME") + "\t");
+				System.out.print(rs.getFloat("ESAL") + "\t");
+				System.out.print(rs.getString("EADDR") + "\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				st.close();
+				con.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
