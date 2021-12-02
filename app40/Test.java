1e0706e7d9bc3ff8aaeaa0e4d84954e771d561fc
Marina Ntogka <marinadoga98@gmail.com>
2021-12-02 17:46:32 +0200

    Add files via upload

---
+++ app40/Test.java
@@ -0,0 +1,41 @@
+package com.marina.app40;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/marinadb", "root", "root");
+			rs = st.executeQuery("select * from emp1");
+			System.out.println("ENO\tENAME\tESAL\tEADDR");
+			System.out.println("----------------------");
+			while(rs.next()) {
+				System.out.print(rs.getInt("ENO")+"\t");
+				System.out.print(rs.getString("ENAME")+"\t");
+				System.out.print(rs.getFloat("ESAL")+"\t");
+				System.out.print(rs.getString("EADDR")+"\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				st.close();
+				con.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
