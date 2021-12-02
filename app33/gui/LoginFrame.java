6660cca6ac13832141dfa5ce016090612db41f74
Marina Ntogka <marinadoga98@gmail.com>
2021-12-02 13:40:04 +0200

    Add files via upload

---
+++ app33/gui/LoginFrame.java
@@ -0,0 +1,14 @@
+package com.marina.app333.gui;
+
+import java.awt.Frame;
+import java.awt.Color;
+
+public class LoginFrame extends Frame {
+	public LoginFrame() {
+		this.setVisible (true);
+		this.setSize(500, 500);
+		this.setTitle("Login Frame");
+		this.setBackground(Color.pink);
+	}
+
+}
---
+++ app33/test/Test.java
@@ -0,0 +1,12 @@
+package com.marina.app333.test;
+
+import com.marina.app333.gui.LoginFrame;
+
+public class Test {
+
+	public static void main(String[] args) {
+		LoginFrame loginFrame = new LoginFrame();
+
+	}
+
+}
