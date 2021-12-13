cff5631790af786c3f1678c3da44ab8245b61134
Marina Ntogka <marinadoga98@gmail.com>
2021-12-13 15:24:56 +0200

    Add files via upload

---
+++ servlet/eclipse/app04/src/main/java/com/marina/app04/servlets/TestServlet.java
@@ -0,0 +1,23 @@
+package com.marina.app04.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1 style='color: red;'>");
+		out.println("Hello Client, this is the response from TestServlet prepared in Eclipse IDE");
+		out.println("</h1></hr></body></html>");
+	}
+
+}
